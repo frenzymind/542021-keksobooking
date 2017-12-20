f7c212b9ce9015b6f8bd79e06daf671ec6cd001c
frenzymidn <frenzymind@mail.ru>
2017-12-20 17:23:47 +0800

    константы цены внесены

--- js/filter.js
+++ js/filter.js
@@ -3,8 +3,17 @@
 window.filter = (function () {
 
   var HOUSING_TYPE_ID = 'housing-type';
+  var HOUSING_PRICE_ID = 'housing-price';
+  var HOUSING_ROOM_ID = 'housing-rooms'
+  var HOUSING_GUEST_ID = 'housing-guests';
 
   var NO_FILTER_VALUE = 'any';
+
+  var MIDDLE_MIN_PRICE = 10000;
+  var MIDDLE_MAX_PRICE = 50000;
+
+  var LOW_PRICE = 10000;
+  var HIGH_PRICE = 50000;
 
   var houseTypeValue;
   var housePriceValue;
